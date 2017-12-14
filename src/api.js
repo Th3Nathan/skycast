9489389cf28c78720997edd07182c0317969f5aa
Nathan Vass <nathanevass@gmail.com>
2017-12-14 00:03:19 -0500

    only heroku url

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import queryString from 'query-string';
 const localUrl = "http://localhost:8080/";
 const herokuUrl = "https://skycast-server.herokuapp.com/";
 
-const url = process.env.NODE_ENV === 'production' ? herokuUrl : localUrl;
+const url = herokuUrl;
 
 const $ = axios.create({withCredentials: true});
 
